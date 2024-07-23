acab183ca5bb81ee2a8f0ca5c3a212353f73cb82
Yanpeng Chen <chenyanpeng1992@outlook.com>
2024-07-23 11:58:41 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -3,17 +3,38 @@
 # step1: download barcode sequences using the script read.GenBank.R, which require that the taxa table must be prepared follow it's rules.
 
 # step2: multiple sequence alignments using mafft
-mkdir 02_mafft
+if [ ! -d 02_mafft ]; then
+  # If the directory does not exist, create it
+  mkdir -p "$directory_path"
+  echo "mafft folder: 02_mafft "
+else
+  echo "02_mafft folder already exists"
+fi
+
 ls 01_data/ | grep fasta | while read a;do echo "mafft --localpair --thread 4 --adjustdirection 01_data/${a} > 02_mafft/${a%.fasta}.mafft.fna";done > mafft.sh
 bash mafft.sh
 sed -i 's/>_R_/>/' 02_mafft/*.mafft.fna
 
 # step3: trim the msa using trimal
-mkdir 03_trimal
+if [ ! -d 03_trimal ]; then
+  # If the directory does not exist, create it
+  mkdir -p 03_trimal
+  echo "03_trimal  folder created "
+else
+  echo "03_trimal folder already exists"
+fi
+
 ls 01_data/ | grep fasta | sed 's/.fasta//' |while read a;do trimal -in 02_mafft/${a}.mafft.fna -gt 0.5 -out 03_trimal/${a%.mafft.fna}.mafft.trimal.fna;done
 
 # step4: find the evolutionary models and concatenate the msa together by the identical identifiers
-mkdir 04_modelfinder
+if [ ! -d 04_modelfinder ]; then
+  # If the directory does not exist, create it
+  mkdir -p 04_modelfinder
+  echo "04_modelfinder folder created"
+else
+  echo "04_modelfinderfolder already exists"
+fi
+
 outgroup_label=Pyrenochaetopsis_uberifbrmis_CBS_142461
 iqtree_modelfinder.py -i 03_trimal/Cucurbitaria_ITS.mafft.trimal.fna \
     03_trimal/Cucurbitaria_LSU.mafft.trimal.fna 03_trimal/Cucurbitaria_TEF1.mafft.trimal.fna 03_trimal/Cucurbitaria_RPB2.mafft.trimal.fna \
@@ -22,8 +43,23 @@ iqtree_modelfinder.py -i 03_trimal/Cucurbitaria_ITS.mafft.trimal.fna \
     --outgroup ${outgroup_label}
 
 # step5: maximum likelihood phylogenetic analysis using iqtree
-mkdir 05_iqtree
+if [ ! -d 05_iqtree ]; then
+  # If the directory does not exist, create it
+  mkdir -p 05_iqtree
+  echo "05_iqtree folder created"
+else
+  echo "05_iqtree already exists"
+fi
+
 nohup iqtree2 -s 04_modelfinder/concatenate.fna --seqtype DNA -o ${outgroup_label} --prefix 05_iqtree/iqtree_ml -T AUTO -p 05_modelfinder/best_scheme.txt --ufboot 1000 --alrt 1000 &
 
 # step6: Bayesian analysis using Mrbayes
+if [ ! -d 06_mrbayes ];then
+  # If the directory does not exist, create it
+  mkdir -p 06_mrbayes
+  echo "06_mrbayes folder created"
+else
+  echo "06_mrbayesalready exists"
+fi
+
 nohup mpirun -n 4 mb < run_mrbayes.sh # if mpirun does not work, please : nohup bash mb < run_mrbayes &
